9793d0b81f3d9d9bcea6e698d5a25f233badd98f
neelima <aithaneelima@gmai.com>
2023-09-04 11:50:03 +0000

    first commit

---
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -0,0 +1,12 @@
+// Style your elements here
+import styled from 'styled-components'
+
+export const ListItem = styled.li`
+  width: 49%;
+  list-style-type: none;
+  @media screen and (min-width: 786px) {
+    width: 24%;
+  }
+`
+
+export const DirectionButton = styled.button``
---
+++ src/components/GradientGenerator/styledComponents.js
@@ -0,0 +1,24 @@
+// Style your elements here
+import styled from 'styled-components/macro'
+
+export const GradientGeneratorContainer = styled.div``
+
+export const ResponsiveContainer = styled.div``
+
+export const Heading = styled.h1``
+
+export const DirectionsDescription = styled.p``
+
+export const GradientDirectionList = styled.ul``
+
+export const ColorPickersDescription = styled.p``
+
+export const ColorPickerContainer = styled.div``
+
+export const CustomInputAndColorContainer = styled.div``
+
+export const ColorValue = styled.p``
+
+export const CustomInput = styled.input``
+
+export const GenerateButton = styled.button``
